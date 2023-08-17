4458f9c97b4985e536d7ebb3f3283261f04bbffb
Adn <nimouhq@gmail.com>
2023-08-17 22:52:50 +0200

    Update data.py

--- tools/data.py
+++ tools/data.py
@@ -1,6 +1,6 @@
 import json
-
-class data:
+CREDIT = "made by 0xadn"
+class importdata:
     def simple(chemin : str, filename : str, name : str  = None):
         with open(f'{chemin}/{filename}.json', 'r') as f:
             if name == None:
@@ -19,3 +19,24 @@ class data:
                 return json.load(f)
             else :
                 return json.load(f)[name_list][name]
+
+class postdata:
+    def simple(chemin : str, filename : str, name : str  = None, value: str  = None):
+        with open(f'{chemin}/{filename}.json', 'r') as f:
+                file =  json.load(f)
+        file[name] = value
+
+        with open(f'{chemin}/{filename}.json', 'w') as f:
+            json.dump(file, f)
+    def list(chemin : str, filename : str, name : str  = None, value: str  = None) :
+        with open(f'{chemin}/{filename}.json', 'r') as f:
+            file =  json.load(f)
+        file[name].append(value)
+        with open(f'{chemin}/{filename}.json', 'w') as f:
+            json.dump(file, f)
+    def tableau(chemin : str, filename : str, name_list : str  = None, name : str = None, value: str  = None):
+        with open(f'{chemin}/{filename}.json', 'r') as f:
+            file =  json.load(f)
+        file[name_list][name] = value    
+        with open(f'{chemin}/{filename}.json', 'w') as f:
+                json.dump(file, f)
