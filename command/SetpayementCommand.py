9ab350f078c9361c6eb967778b8e5de83f50fe2c
Adn <nimouhq@gmail.com>
2023-09-03 11:25:40 +0200

    Vouch Logs Command

--- command/SetpayementCommand.py
+++ command/SetpayementCommand.py
@@ -18,44 +18,47 @@ class setpayement(commands.Cog):
 
     @commands.command()
     async def setpayement(self,ctx, select : str, value : str, value_op :str  = None):
-        if select == "paypal":
-            postdata.tableau(chemin= 'database/', filename= 'payement', name_list= 'paypal', name= 'link', value= value)
-            postdata.tableau(chemin= 'database/', filename= 'payement', name_list= 'paypal', name= 'email', value= value_op)
-            
-            embed = discord.Embed(
+        if Check.owner(str(ctx.message.author.id)) == True :
+            if select == "paypal":
+                postdata.tableau(chemin= 'database/', filename= 'payement', name_list= 'paypal', name= 'link', value= value)
+                postdata.tableau(chemin= 'database/', filename= 'payement', name_list= 'paypal', name= 'email', value= value_op)
                 
-                title = f"Payement setup : {select}",
+                embed = discord.Embed(
+                    
+                    title = f"Payement setup : {select}",
+                    
+                    description = f"> Payement link : {value}\n> Payement email : {value_op}",
+
+                    color = discord.Color.blue()
+                )
+                embed.set_footer(text= CREDIT)
+                embed.set_thumbnail(url="https://media.discordapp.net/attachments/1135971296774193223/1135972383950381137/paypal-logo.gif")
                 
-                description = f"> Payement link : {value}\n> Payement email : {value_op}",
+                await ctx.send(embed=embed)
 
-                color = discord.Color.blue()
-            )
-            embed.set_footer(text= CREDIT)
-            embed.set_thumbnail(url="https://media.discordapp.net/attachments/1135971296774193223/1135972383950381137/paypal-logo.gif")
-            
-            await ctx.send(embed=embed)
+            elif select == "eth" or "btc" or "ltc"  :
+                postdata.tableau(chemin= 'database/', filename= 'payement',name_list='crypto', name= select, value= value)
 
-        elif select == "eth" or "btc" or "ltc"  :
-            postdata.tableau(chemin= 'database/', filename= 'payement',name_list='crypto', name= select, value= value)
+                embed = discord.Embed(
+                    title = f"Payement setup : {select}",
+                                
+                    description = f"> {select} addy  : {value}",    
 
-            embed = discord.Embed(
-                title = f"Payement setup : {select}",
-                            
-                description = f"> {select} addy  : {value}",    
+                )
+                if select == "eth" :
+                    embed.color = 0x000000
+                    embed.set_thumbnail(url = "https://media.discordapp.net/attachments/1135971296774193223/1135972755385364481/eth-logo.gif")
+                elif select == "btc" :
+                    embed.color = 0xFFFF00
+                    embed.set_thumbnail(url = "https://media.discordapp.net/attachments/1135971296774193223/1135972383522566255/btc-logo.gif")
+                elif select == "ltc" :
+                    embed.color = discord.Color.blue()
+                    embed.set_thumbnail(url ="https://media.discordapp.net/attachments/1135971296774193223/1135972383140872242/ltc-logo.gif")
+                
+                embed.set_footer(text= CREDIT)
 
-            )
-            if select == "eth" :
-                embed.color = 0x000000
-                embed.set_thumbnail(url = "https://media.discordapp.net/attachments/1135971296774193223/1135972755385364481/eth-logo.gif")
-            elif select == "btc" :
-                embed.color = 0xFFFF00
-                embed.set_thumbnail(url = "https://media.discordapp.net/attachments/1135971296774193223/1135972383522566255/btc-logo.gif")
-            elif select == "ltc" :
-                embed.color = discord.Color.blue()
-                embed.set_thumbnail(url ="https://media.discordapp.net/attachments/1135971296774193223/1135972383140872242/ltc-logo.gif")
-            
-            embed.set_footer(text= CREDIT)
-
-            await ctx.send(embed=embed)
+                await ctx.send(embed=embed)
+        else : 
+            await ctx.send("you are not authorized to use this command")
 
 
---
+++ command/VouchLogs.py
@@ -0,0 +1,51 @@
+import discord, datetime
+
+from discord import app_commands
+from discord.ext import commands
+from discord import ui
+from tools.data import *
+from tools.check import *
+
+async def setup(client):
+    await client.add_cog(setvouchlogs(client))
+
+class setvouchlogs(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def setvouchlog(self, ctx, channel : discord.TextChannel):
+        if Check.owner(str(ctx.message.author.id)) == True :
+            
+            with open('database/logs.json', 'r') as f :
+                file = json.load(f)
+            
+            file["vouch"] = channel.id
+
+            with open('database/logs.json', 'w') as f:
+                json.dump(file, f, indent=2)
+            
+            await ctx.send(f"logs succesfuly setup in <#{channel.id}>")
+            await channel.send("Vouch Setup in this **channel**")
+
+        else :
+            await ctx.send("you are not authorized to use this command")
+    
+    @app_commands.command(name = "setvouchlog", description="setup vouch log channel")
+    async def setvouchlog(self, interaction: discord.Interaction, channel: discord.TextChannel):
+        if Check.owner(str(interaction.user.id)) == True :
+            
+            with open('database/logs.json', 'r') as f :
+                file = json.load(f)
+            
+            file["vouch"] = channel.id
+
+            with open('database/logs.json', 'w') as f:
+                json.dump(file, f, indent=2)
+            
+            await interaction.response.send_message(f"logs succesfuly setup in <#{channel.id}>", ephemeral= True)
+            await channel.send("Vouch Setup in this **channel**")
+
+        else :
+            await interaction.response.send_message("you are not authorized to use this command", ephemeral= True)        
+    
