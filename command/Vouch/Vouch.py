d8937e1f29c4d55b7261957548dc8957d6231b98
Adn <nimouhq@gmail.com>
2023-09-11 21:07:06 +0200

    Update Vouch.py

--- command/Vouch/Vouch.py
+++ command/Vouch/Vouch.py
@@ -1,10 +1,15 @@
-import discord, datetime
+import discord, datetime, enum
 
 from discord import app_commands
 from discord.ext import commands
 from discord import ui
 from tools.data import *
 from tools.check import *
+
+class Type(enum.Enum):
+
+    Good = "good" 
+    Bad = "bad"
 
 async def setup(client):
     await client.add_cog(VouchCommandPhone(client))
@@ -17,13 +22,13 @@ class VouchCommandPhone(commands.Cog):
 
 
     @app_commands.command(name= "vouch", description= "vouch command",)
-    async def vouch(self, interaction: discord.Interaction, user: discord.User, type : str  = None, product : str = None,  quantity : str = None , price : str = None):
+    async def vouch(self, interaction: discord.Interaction, user: discord.User, type : Type, product : str = None,  quantity : str = None , price : str = None):
             with open('database/vouch.json','r') as f:
                 file = json.load(f)
             
             file[f"vouch : {datetime.datetime.utcnow()}"] = {
                 "VouchIdUser" : user.id,
-                "VouchType" : type,
+                "VouchType" : type.value,
                 "VouchUser" : user.name,
                 "VouchValue" : product + quantity + price
             }
@@ -36,11 +41,11 @@ class VouchCommandPhone(commands.Cog):
 
             channel = interaction.guild.get_channel(int(file["vouch"]))
             embed = discord.Embed(title=f"Vouch for {user.name}",
-                                description=f"\n> type : **{type}**"+ f"\n> vouch  : {product}   {quantity} + {price}")
+                                description=f"\n> type : **{type.value}**"+ f"\n> vouch  : {product}   {quantity} + {price}")
             
-            if type == "good" :
+            if type.value == "good" :
                 embed.color = discord.Color.green()
-            if type == "bad" :
+            if type.value == "bad" :
                 embed.color = discord.Color.red()
             
             embed.set_author(name=user.name, url= f"https://discord.com/users/{user.id}")
