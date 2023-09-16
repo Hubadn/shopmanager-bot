293a5d7354363bbbd1d4ffa54bc70057f19eab34
Adn <nimouhq@gmail.com>
2023-09-16 19:58:30 +0200

    Update Vouch.py

--- command/Vouch/Vouch.py
+++ command/Vouch/Vouch.py
@@ -23,34 +23,38 @@ class VouchCommandPhone(commands.Cog):
 
     @app_commands.command(name= "vouch", description= "vouch command",)
     async def vouch(self, interaction: discord.Interaction, user: discord.User, type : Type, product : str = None,  quantity : str = None , price : str = None):
+            succes = True
             with open('database/vouch.json','r') as f:
                 file = json.load(f)
             
-            file[f"vouch : {datetime.datetime.utcnow()}"] = {
-                "VouchIdUser" : user.id,
-                "VouchType" : type.value,
-                "VouchUser" : user.name,
-                "VouchValue" : product + quantity + price
-            }
+            try :
+                file[f"vouch : {user.id}"]["content"].append(f"{product} {quantity} {price}")
+                file[f"vouch : {user.id}"]["type"].append(type.value)
+            except :
+                succes =  False
 
             with open('database/vouch.json','w') as f:
                 json.dump(file, f , indent = 2)
             
-            with open('database/logs.json', 'r') as f:
-                file = json.load(f)
+            if succes == True:    
+                with open('database/logs.json', 'r') as f:
+                    file = json.load(f)
 
-            channel = interaction.guild.get_channel(int(file["vouch"]))
-            embed = discord.Embed(title=f"Vouch for {user.name}",
-                                description=f"\n> type : **{type.value}**"+ f"\n> vouch  : {product}   {quantity} + {price}")
-            
-            if type.value == "good" :
-                embed.color = discord.Color.green()
-            if type.value == "bad" :
-                embed.color = discord.Color.red()
-            
-            embed.set_author(name=user.name, url= f"https://discord.com/users/{user.id}")
-            try : 
-                await channel.send(embed = embed)
-            except :
-                pass
-            await interaction.response.send_message(f"Thank you, {user.name}", ephemeral=True)          
+                channel = interaction.guild.get_channel(int(file["vouch"]))
+                embed = discord.Embed(title=f"Vouch for {user.name}",
+                                    description=f"\n> type : **{type.value}**"+ f"\n> vouch  : {product}   {quantity} + {price}")
+                
+                if type.value == "good" :
+                    embed.color = discord.Color.green()
+                if type.value == "bad" :
+                    embed.color = discord.Color.red()
+                
+                embed.set_author(name=user.name, url= f"https://discord.com/users/{user.id}")
+                try : 
+                    await channel.send(embed = embed)
+                except :
+                    pass
+                await interaction.response.send_message(f"Thank you, {user.name}", ephemeral=True)
+            if succes == False :
+                await user.send(f" you are not in database please check if he use command /setup_vouch_profile and revouch")  
+                await interaction.response.send_message("error")    
